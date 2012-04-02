f10470c68aea7feb2967b14eba574c9ae8b0f6db
Weinan Li <l.weinan@gmail.com>
2012-04-02 20:47:55 +0800

    add multicast pieces

---
+++ src/main/java/net/bluedash/snippets/multicast/MulticastSniffer.java
@@ -0,0 +1,13 @@
+package net.bluedash.snippets.multicast;
+
+import java.net.InetAddress;
+
+public class MulticastSniffer {
+    public static void main(String[] args) {
+        InetAddress group = null;
+        int port = 0;
+
+
+
+    }
+}
