a79c24de4de71d2d207759e77266c897d7d8c78f
Weinan Li <weli@redhat.com>
2012-04-09 18:36:29 +0800

    add ClassLoadingMethods

---
+++ src/main/java/net/bluedash/snippets/pieces/ClassLoadingMethods.java
@@ -0,0 +1,20 @@
+package net.bluedash.snippets.pieces;
+
+/*
+ * mvn install
+ * mvn -q exec:java -Dexec.mainClass="net.bluedash.snippets.pieces.ClassLoadingMethods" -Dexec.args="-Dsun.lang.ClassLoader.allowArraySyntax=true" 
+ */
+public class ClassLoadingMethods {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		/* 方法 1: 使用 .class */
+		Class c = String[].class;
+
+		/* 方法 2: 使用实例的getClass()方法 */
+		c = new String[1].getClass();
+
+		/* 方法 3: 使用 Class.forName */
+		c = Class.forName("[Ljava.lang.String;");
+	}
+
+}
