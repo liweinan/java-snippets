03245869fc0c231bfd763a4e28344b68ae8941a4
Weinan Li <weli@redhat.com>
2013-02-19 00:20:07 +0800

    more codes on jackson

--- src/main/java/net/bluedash/snippets/jackson/Main.java
+++ src/main/java/net/bluedash/snippets/jackson/Main.java
@@ -1,9 +1,13 @@
 package net.bluedash.snippets.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.bluedash.snippets.io.StringOutputStream;
 
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
@@ -14,5 +18,28 @@ public class Main {
         ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
         User user = mapper.readValue(new File("target/classes/user.json"), User.class);
         System.out.println(user);
+
+        //raw
+        Map<String,Object> userData = new HashMap<String,Object>();
+        Map<String,String> nameStruct = new HashMap<String,String>();
+        nameStruct.put("first", "Joe");
+        nameStruct.put("last", "Sixpack");
+        userData.put("name", nameStruct);
+        userData.put("gender", "MALE");
+        userData.put("verified", Boolean.FALSE);
+        userData.put("userImage", "Rm9vYmFyIQ==");
+
+        StringOutputStream out = new StringOutputStream();
+        mapper.writeValue(out, userData);
+        System.out.println(out.toString());
+
+        //array
+        out = out.renew();
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Tomato");
+        fruits.add("Cucumber");
+        mapper.writeValue(out, fruits);
+        System.out.println(out.toString());
     }
 }
