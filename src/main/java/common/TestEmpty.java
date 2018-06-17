ed2c32c1edc29a2c5496d09ba890b396ddb7ab11
Weinan Li <l.weinan@gmail.com>
2018-06-18 04:30:37 +0800

    rxjava stuff

---
+++ src/main/java/common/TestEmpty.java
@@ -0,0 +1,13 @@
+package common;
+
+public class TestEmpty {
+    static void passNull(Object nil) {
+        if (nil == null)
+            nil = new Object();
+        System.out.println(nil);
+    }
+
+    public static void main(String[] args) {
+        passNull(null);
+    }
+}
