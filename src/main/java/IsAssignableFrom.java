d15d09f3241a88658018f96ee4ed9ad26bd4e70a
Weinan Li <l.weinan@gmail.com>
2019-02-01 16:40:10 +0800

    macos_install_hadoop.sh

--- src/main/java/IsAssignableFrom.java
+++ src/main/java/IsAssignableFrom.java
@@ -4,9 +4,9 @@ public class IsAssignableFrom {
 
    public static void main(String[] args) {
       System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-      System.out.println(Foo.class.isAssignableFrom(FooImpl.class));
+      System.out.println(Foo.class.isAssignableFrom(FooImpl.class)); // correct
       System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-      System.out.println(FooImpl.class.isAssignableFrom(Foo.class));
+      System.out.println(FooImpl.class.isAssignableFrom(Foo.class)); // wrong
       System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
    }
 }
