575487d787c58b5268f03e829e88fbbd5a25d685
Weinan Li <l.weinan@gmail.com>
2019-03-23 00:07:38 +0800

    .

---
+++ src/main/java/concurrent/CompletableFutureDemo.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.jgroups.util.Util.assertEquals;
+
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Hello";
+        });
+
+        CompletableFuture<String> future = completableFuture.thenApply(s -> s + " World");
+
+        assertEquals("Hello World", future.get());
+    }
+}
