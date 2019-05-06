af43de4bcc5377c75104dc06723c33b33e1f6655
weli <l.weinan@gmail.com>
2019-05-06 14:47:59 +0800

    .

--- src/main/java/io/weli/LambdaBestPractices.java
+++ src/main/java/io/weli/LambdaBestPractices.java
@@ -6,9 +6,23 @@ import java.util.function.Function;
  * https://www.baeldung.com/java-8-lambda-expressions-tips
  */
 public class LambdaBestPractices {
+
+   @FunctionalInterface
+   public interface Foo {
+      String method(String v);
+
+      default String defaultMethod(String v) {
+         return v;
+      }
+   }
+
    public static void main(String[] args) {
       Function<String, String> fn = v -> v.toUpperCase();
       System.out.println(fn.apply("hello"));
+
+      Foo f = v -> v.toUpperCase();
+      System.out.println(f.method("hello"));
+      System.out.println(f.defaultMethod("hello"));
 
    }
 }
