f94ec5003e59f9e8230e31eab2382c837b488f3b
weli <l.weinan@gmail.com>
2019-06-04 18:16:41 +0800

    .

--- src/test/java/io/weli/OptionalTest.java
+++ src/test/java/io/weli/OptionalTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static org.jgroups.util.Util.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 // https://www.baeldung.com/java-optional
@@ -28,4 +29,43 @@ public class OptionalTest {
       Optional<String> optionalName = Optional.ofNullable(name);
       assertTrue(optionalName.isPresent());
    }
+
+   @Test
+   public void givenNull_whenCreatesNullable_thenCorrect() {
+      String name = null;
+      Optional<String> opt = Optional.ofNullable(name);
+      assertFalse(opt.isPresent());
+   }
+
+   @Test
+   public void givenOptional_whenIsPresentWorks_thenCorrect() {
+      Optional<String> opt = Optional.of("Baeldung");
+      assertTrue(opt.isPresent());
+
+      opt = Optional.ofNullable(null);
+      assertFalse(opt.isPresent());
+   }
+
+   @Test
+   public void givenOptional_whenIfPresentWorks_thenCorrect() {
+      Optional<String> opt = Optional.of("baeldung");
+      opt.ifPresent(name -> System.out.println(name.length()));
+   }
+
+   @Test
+   public void whenOrElseWorks_thenCorrect() {
+      String nullName = null;
+      String name = Optional.ofNullable(nullName).orElse("john");
+      assertEquals("john", name);
+   }
+
+
+   @Test
+   public void whenOrElseGetWorks_thenCorrect() {
+      String nullName = null;
+      String name = Optional.ofNullable(nullName).orElseGet(() -> "john");
+      assertEquals("john", name);
+   }
+
+   
 }
