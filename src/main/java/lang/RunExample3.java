8619470514ff977317b7911e21e60ae1b21c7ea4
weli <l.weinan@gmail.com>
2019-06-07 16:51:23 +0800

    .

---
+++ src/main/java/lang/RunExample3.java
@@ -0,0 +1,38 @@
+package lang;
+
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/*
+ * https://www.logicbig.com/tutorials/core-java-tutorial/java-multi-threading/completion-stage-and-completable-future.html
+ */
+public class RunExample3 {
+
+   public static void main(String[] args) {
+      ExecutorService executor = Executors.newFixedThreadPool(3);
+
+      CompletableFuture.runAsync(() -> performTask("first stage"), executor)
+              .thenRun(() -> performTask("second stage"))
+              .thenRunAsync(() -> performTask("third stage"), executor)
+              .join();//waits until task is completed
+      System.out.println("main exiting");
+      executor.shutdown();
+   }
+
+   private static void performTask(String stage) {
+      System.out.println("---------");
+      System.out.printf("stage: %s, time before task: %s, thread: %s%n",
+              stage, LocalTime.now(), Thread.currentThread().getName());
+      try {
+         //simulating long task
+         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+      System.out.printf("stage: %s, time after task: %s, thread: %s%n",
+              stage, LocalTime.now(), Thread.currentThread().getName());
+   }
+
+}
