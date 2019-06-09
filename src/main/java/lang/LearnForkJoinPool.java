29911215cd465a2b3c1975f3d11c7151d2de6db0
weli <l.weinan@gmail.com>
2019-06-09 11:57:53 +0800

    .

---
+++ src/main/java/lang/LearnForkJoinPool.java
@@ -0,0 +1,9 @@
+package lang;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class LearnForkJoinPool {
+public static void main(String[] args) {
+   ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+}
+}
