81ae5d74102fa1386049adca26d192c64f2a4e5b
阿男 <l.weinan@gmail.com>
2019-07-18 14:44:35 +0800

    .

--- src/main/java/lang/PlayWithCopyOnWriteArrayList.java
+++ src/main/java/lang/PlayWithCopyOnWriteArrayList.java
@@ -15,9 +15,19 @@ public class PlayWithCopyOnWriteArrayList {
 
         Iterator<Integer> iterator = numbers.iterator();
 
+        numbers.add(10);
+
         List<Integer> result = new LinkedList<>();
         iterator.forEachRemaining(result::add);
 
         assertThat(result).containsOnly(1, 3, 5, 8);
+
+        Iterator<Integer> iterator2 = numbers.iterator();
+        List<Integer> result2 = new LinkedList<>();
+
+        iterator2.forEachRemaining(result2::add);
+
+        assertThat(result2).containsOnly(1, 3, 5, 8, 10);
+
     }
 }
