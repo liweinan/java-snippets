08a4e47bc8b2668d602b2dc8de72ce2667fe16ba
阿男 <l.weinan@gmail.com>
2019-07-19 14:36:08 +0800

    .

--- src/main/java/lang/CachedOrNotCached.java
+++ src/main/java/lang/CachedOrNotCached.java
@@ -9,13 +9,19 @@ public class CachedOrNotCached {
 
       StringBuilder builder = new StringBuilder();
       builder.append("a");
-      Object obj = builder.toString().hashCode();
+      Object obj = builder.toString();
       System.out.println(obj.hashCode());
+      System.out.println(Integer.valueOf(97).hashCode());
 
       Map<Object, Object> map = new HashMap<>();
       map.put(obj, 1);
       map.put("a", 2);
 
       System.out.println(map.toString());
+
+      System.out.println(System.identityHashCode(Integer.valueOf(97)));
+      System.out.println(System.identityHashCode("a"));
+      System.out.println(System.identityHashCode(obj.hashCode()));
+
    }
 }
