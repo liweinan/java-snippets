3bb285f0e70395e0438434f41b5ea67a95d9d35a
Weinan Li <l.weinan@gmail.com>
2019-07-29 00:30:52 +0800

    .

--- src/main/java/io/weli/concurrent/ClientClient.java
+++ src/main/java/io/weli/concurrent/ClientClient.java
@@ -9,7 +9,52 @@ import java.util.concurrent.TimeoutException;
 public class ClientClient {
 
 
+   public static void main(String[] args) throws Exception {
+
+
+      // 主线程要得到异步的Foo的instance
+      AsyncClient asyncClient = new AsyncClient();
+
+      Future obj = asyncClient.call();
+
+      // 可以干别的不影响
+      System.out.println("主线程在干别的");
+
+
+      // 得到foo的处理结果，调用`getResult()`的时候为同步。
+      System.out.println(obj.get()); // 会block住主线程
+
+
+   }
+
    public static class AsyncClient {
+
+      CountDownLatch latch = new CountDownLatch(1);
+      private Future val;
+
+      public Future call() {
+         System.out.println("启动新线程处理Foo的instance");
+         FutureObject obj = new FutureObject();
+         new Thread() {
+            @Override
+            public void run() {
+               try {
+                  Thread.sleep(1000 * 3);
+                  obj.setVal("Hello, world");
+                  super.run();
+                  latch.countDown();
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }.start();
+         return obj;
+      }
+
+//      public String getResult() throws InterruptedException {
+//         latch.await();
+//         return val;
+//      }
 
       class FutureObject implements Future {
 
@@ -45,53 +90,6 @@ public class ClientClient {
             return null;
          }
       }
-
-      CountDownLatch latch = new CountDownLatch(1);
-
-      private Future val;
-
-//      public String getResult() throws InterruptedException {
-//         latch.await();
-//         return val;
-//      }
-
-      public Future call() {
-         System.out.println("启动新线程处理Foo的instance");
-         FutureObject obj = new FutureObject();
-         new Thread() {
-            @Override
-            public void run() {
-               try {
-                  Thread.sleep(1000 * 3);
-                  obj.setVal("Hello, world");
-                  super.run();
-                  latch.countDown();
-               } catch (InterruptedException e) {
-                  e.printStackTrace();
-               }
-            }
-         }.start();
-         return obj;
-      }
-   }
-
-
-   public static void main(String[] args) throws Exception {
-
-
-      // 主线程要得到异步的Foo的instance
-      AsyncClient asyncClient = new AsyncClient();
-
-      Future obj = asyncClient.call();
-
-      // 可以干别的不影响
-      System.out.println("主线程在干别的");
-
-
-      // 得到foo的处理结果，调用`getResult()`的时候为同步。
-      System.out.println(obj.get()); // 会block住主线程
-
-
    }
 
 }
