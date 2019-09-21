6854e2eb8d3975da18a8dd2c20c50d1fa777c4c6
阿男 <l.weinan@gmail.com>
2019-09-21 17:27:25 +0800

    .

---
+++ src/main/java/io/weli/lang/NullNull.java
@@ -0,0 +1,26 @@
+package io.weli.lang;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NullNull {
+   static NullNull pass(Object obj) {
+      return (NullNull) obj;
+   }
+   public static void main(String[] args) {
+      System.out.println(pass(null));
+      System.out.println(pass("aaaa"));
+
+      List<String> lst = new ArrayList<>();
+      lst.add("a");
+
+
+      List lst2 = new ArrayList<>();
+      lst2.add(1);
+      lst2.add(new Object());
+
+//      String str = (String) lst2.get(0);
+
+   }
+}
