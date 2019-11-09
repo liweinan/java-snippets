e0d1d05f98fdfba15073a8b1120fe00431f0897d
阿男 <l.weinan@gmail.com>
2019-11-09 11:42:37 +0800

    .

---
+++ src/main/java/io/weli/lang/LambdasAndStreams.java
@@ -0,0 +1,15 @@
+package io.weli.lang;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+//https://learning.oreilly.com/library/view/effective-java-3rd/9780134686097/ch7.xhtml#ch7
+public class LambdasAndStreams {
+   public static void main(String[] args) {
+//      https://www.techiedelight.com/initialize-list-java-single-line/
+      List<String> fruits = Arrays.asList("Apple", "Orange", "Tomato");
+      Collections.sort(fruits, Comparator.comparingInt(String::length));
+   }
+}
