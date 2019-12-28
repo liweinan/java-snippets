0dfb330a0ab7bec8c5837e6fc8b21f24c7da5257
阿男 <l.weinan@gmail.com>
2019-12-28 23:09:29 +0800

    .

--- src/main/java/io/weli/alg/BiListDemo.java
+++ src/main/java/io/weli/alg/BiListDemo.java
@@ -20,6 +20,14 @@ public class BiListDemo {
 
         T rpop();
 
+        T shift();
+
+        void unshift(T obj);
+
+        T pop();
+
+        void push(T obj);
+
         int length();
 
         Optional reduce(BinaryOperator bifunc);
@@ -35,10 +43,22 @@ public class BiListDemo {
         List copyStore();
 
         void dump();
+
+        T get(int idx);
     }
 
     @NotThreadSafe
     static class BiListImpl<T> implements BiList<T> {
+
+
+        @Override
+        public T get(int idx) {
+            if (store.size() == 0)
+                return null;
+            if (idx < 0 || idx > store.size() - 1)
+                return null;
+            return store.get(idx);
+        }
 
         @Override
         public List copyStore() {
@@ -94,6 +114,27 @@ public class BiListDemo {
         }
 
         @Override
+        public T shift() {
+            return lpop();
+        }
+
+        @Override
+        public void unshift(T obj) {
+            lpush(obj);
+        }
+
+
+        @Override
+        public T pop() {
+            return rpop();
+        }
+
+        @Override
+        public void push(T obj) {
+            rpush(obj);
+        }
+
+        @Override
         public int length() {
             return store.size();
         }
@@ -135,7 +176,11 @@ public class BiListDemo {
 
         @Override
         public void dump() {
-            System.out.println(":::DUMP::: " + store.toString());
+            System.out.print(":::DUMP::: ");
+            for (Object obj : store) {
+                System.out.print(obj.toString());
+                System.out.print(", ");
+            }
         }
     }
 
@@ -177,5 +222,116 @@ public class BiListDemo {
             System.out.println("_x: " + x);
             return x.toString().equals("x");
         }));
+
+        int[] _data = {-57, 9, -72, -72, -62, 45, -97, 24, -39, 35, -82, -4, -63, 1, -93, 42, 44, 1, -75, -25, -87, -16, 9, -59, 20};
+
+        BiList<Integer> lst4 = new BiListImpl();
+        for (int i : _data) {
+            lst4.push(i);
+        }
+        lst4.dump();
+
+        System.out.println(maxSubArray(lst4));
     }
+
+    static BiList<Integer> loop(BiList<Integer> nums, Just<Integer> maxSum) {
+        System.out.println("------------------loop nums: ");
+        nums.dump();
+        System.out.println("------max sum: " + maxSum.getObj());
+        // step 1: trim all negative numbers in head
+        var num = nums.shift();
+        while (num <= 0 && nums.length() > 0) {
+            num = nums.shift();
+        }
+        nums.unshift(num);
+        System.out.println("left trimmed: ");
+        nums.dump();
+        // step 2: trim all negative numbers in tail
+        num = nums.pop();
+        while (num <= 0 && nums.length() > 0) {
+            num = nums.pop();
+        }
+        nums.push(num);
+        System.out.println("right trimmed: ");
+        nums.dump();
+
+        var left = 0;
+        var n = nums.shift();
+        var right = 0;
+        // step 3: sum left nums
+        while (n != null && n >= 0) {
+            left += n;
+            n = nums.shift();
+        }
+        if (n != null) nums.unshift(n);
+        if (left > 0) nums.unshift(left);
+        System.out.println("left added: ");
+        nums.dump();
+        maxSum.setObj(Math.max(left, maxSum.getObj()));
+
+        if (n != null) {
+            System.out.println("// n must be < 0: " + n);
+            if (left + n > 0) {
+                nums = new BiListImpl<>(left + n, nums.slice(2));
+            } else {
+                nums = nums.slice(2);
+            }
+        }
+        System.out.println("left calc nums: ");
+        nums.dump();
+        // step 4: sum right nums
+        n = nums.pop();
+        while (n != null && n >= 0) {
+            right += n;
+            n = nums.pop();
+        }
+        if (n != null) nums.push(n);
+        if (right > 0) nums.push(right);
+        maxSum.setObj(Math.max(right, maxSum.getObj()));
+        if (n != null) {
+            System.out.println("// n must be < 0: " + n);
+            if (right + n > 0) {
+                nums = new BiListImpl<>(nums.slice(0, nums.length() - 2), right + n);
+            } else {
+                nums = nums.slice(0, nums.length() - 2);
+            }
+        }
+        System.out.println("right calc nums: ");
+        nums.dump();
+        return nums;
+    }
+
+
+    static class Just<T> {
+        private T obj;
+
+        public T getObj() {
+            return obj;
+        }
+
+        public void setObj(T obj) {
+            this.obj = obj;
+        }
+    }
+
+    static int maxSubArray(BiList<Integer> nums) {
+        Just<Integer> maxSum = new Just<>();
+        maxSum.setObj(Integer.MIN_VALUE);
+
+        if (nums.every(n -> (Integer) n <= 0))
+            return (int) nums.reduce((p, c) -> {
+                        return Math.max((int) p, (int) c);
+                    }
+            ).get();
+
+        while (nums.length() > 1) {
+            nums = loop(nums, maxSum);
+        }
+        System.out.println("---FINAL---");
+
+        int _final = Math.max(nums.get(0), maxSum.getObj());
+        System.out.println(_final);
+        return _final;
+    }
+
 }
