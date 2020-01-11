b21e07fb89b45a1d8f35278d2ab87bbe25caf66b
阿男 <l.weinan@gmail.com>
2020-01-11 08:16:49 +0800

    update

--- src/main/java/io/weli/beanutils/Play.java
+++ src/main/java/io/weli/beanutils/Play.java
@@ -9,12 +9,21 @@ import java.util.List;
 // https://www.baeldung.com/apache-commons-beanutils
 public class Play {
     public static void main(String[] args) throws Exception {
+
         Course course = new Course();
         String name = "Computer Science";
         List<String> codes = Arrays.asList("CS", "CS01");
 
         PropertyUtils.setSimpleProperty(course, "name", name);
         PropertyUtils.setSimpleProperty(course, "codes", codes);
+
+        System.out.println(course);
+
+        Student student = new Student();
+        String studentName = "Joe";
+        student.setName(studentName);
+
+        PropertyUtils.setMappedProperty(course, "enrolledStudent(ST-1)", student);
 
         System.out.println(course);
     }
