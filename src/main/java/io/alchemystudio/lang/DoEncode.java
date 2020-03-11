e647470278411adbdea77aebdb6e9f31033e2397
Weinan Li <l.weinan@gmail.com>
2020-03-11 21:31:54 +0800

    .

--- src/main/java/io/alchemystudio/lang/DoEncode.java
+++ src/main/java/io/alchemystudio/lang/DoEncode.java
@@ -7,7 +7,7 @@ public class DoEncode {
     public static void main(String[] args) throws Exception {
         System.out.println(
                 URLEncoder
-                        .encode("http://0.0.0.0:8080/users?paging=0%2c-1sp137%3Cscript%3Ealert(1)%3C%2fscript%3Emzx4u",
+                        .encode("http://0.0.0.0:8080/users?paging=0%2c-1sp137<script>Ealert(1)%3C%2fscript%3Emzx4u",
                                 StandardCharsets.UTF_8.toString()));
 
     }
