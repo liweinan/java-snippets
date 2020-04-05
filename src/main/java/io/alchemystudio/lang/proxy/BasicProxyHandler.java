8dd2704ddaf643fbace7f24e9a118b9f197776c5
阿男 <l.weinan@gmail.com>
2020-04-05 19:17:14 +0800

    Add field proxy handler (#27)
    * add cglib example
    
    * .
    
    * .
    
    * p2
    
    * AddFieldProxyHandler
    
    * update
    
    * update doc
    
    * .

--- src/main/java/io/alchemystudio/lang/proxy/BasicProxyHandler.java
+++ src/main/java/io/alchemystudio/lang/proxy/BasicProxyHandler.java
@@ -8,6 +8,14 @@ public class BasicProxyHandler implements InvocationHandler {
 
     private Object original;
 
+    public Object getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(Object original) {
+        this.original = original;
+    }
+
     public BasicProxyHandler(Object original) {
         this.original = original;
     }
@@ -15,9 +23,9 @@ public class BasicProxyHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("method: " + method.getName());
-        method.invoke(original, args);
+        Object returnVal = method.invoke(original, args);
         System.out.println("done");
-        return null;
+        return returnVal;
     }
 
     interface Foo {
