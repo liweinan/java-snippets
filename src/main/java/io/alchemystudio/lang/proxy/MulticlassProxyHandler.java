80ab921b9003d8d1c964aa2d6f48356d0e68eeb8
阿男 <l.weinan@gmail.com>
2020-04-09 15:01:21 +0800

    update

--- src/main/java/io/alchemystudio/lang/proxy/MulticlassProxyHandler.java
+++ src/main/java/io/alchemystudio/lang/proxy/MulticlassProxyHandler.java
@@ -5,47 +5,49 @@ import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
+// 一个proxy代理多个classes
 public class MulticlassProxyHandler extends BasicProxyHandler {
 
-    Map<Class, Object> proxiedObjects = new HashMap<>();
-
+    // 存放要代理的classes
+    Map<Class, Object> delegates = new HashMap<>();
 
     public MulticlassProxyHandler(Object... targets) {
         for (Object o : targets) {
-            proxiedObjects.put(o.getClass(), o);
+            // interfaces作为keys
+            delegates.put(o.getClass().getInterfaces()[0], o);
         }
     }
 
-    public MulticlassProxyHandler(Object original) {
-        super(original);
-    }
-
     interface Bar {
-        public void hello();
+        void bar(String val);
     }
 
     static class BarImpl implements Bar {
         @Override
-        public void hello() {
-            System.out.println("Hello, bar!");
+        public void bar(String val) {
+            System.out.println(val);
         }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-        System.out.println(Proxy.isProxyClass(proxy.getClass()));
-        System.out.println(method.getDeclaringClass().toString());
-        return null;
-
+        // 取出对应class的delegate，并进行invoke()。
+        return method.invoke(delegates.get(method.getDeclaringClass()), args);
     }
 
     public static void main(String[] args) {
         Foo foo = new FooImpl();
         Bar bar = new BarImpl();
-        Proxy.newProxyInstance(
+
+        // 一个proxy同时代理`foo`和`bar`。
+        Object proxy = Proxy.newProxyInstance(
                 MulticlassProxyHandler.class.getClassLoader(),
                 new Class[]{Foo.class, Bar.class},
                 new MulticlassProxyHandler(foo, bar));
+
+        // 把proxy给cast成foo或者bar来使用。
+        ((Foo) proxy).echo("Hello, foo!");
+        ((Bar) proxy).bar("Hello, bar!");
+
     }
 }
