f8b79bd67b5e1bd376a5041493d839d008983aea
阿男 <l.weinan@gmail.com>
2020-04-12 07:27:58 +0800

    update (#32)

--- src/main/java/io/alchemystudio/classloader/MyClass.java
+++ src/main/java/io/alchemystudio/classloader/MyClass.java
@@ -2,7 +2,9 @@ package io.alchemystudio.classloader;
 
 public class MyClass {
     public static void main(String[] args) throws Exception {
-        Class clazz = Class.forName("io.alchemystudio.classloader.MyClass");
+        Class clazz = ClassLoader
+                .getSystemClassLoader()
+                .loadClass("io.alchemystudio.classloader.MyClass");
         MyClass myClass = (MyClass) clazz.getDeclaredConstructor().newInstance();
         myClass.sayHello();
     }
