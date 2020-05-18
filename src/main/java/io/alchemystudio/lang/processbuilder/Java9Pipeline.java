c64cac65af4f262b0883ba0aa94113f7c4618928
阿男 <l.weinan@gmail.com>
2020-05-18 17:42:17 +0800

    Java9Pipeline

--- src/main/java/io/alchemystudio/lang/processbuilder/Java9Pipeline.java
+++ src/main/java/io/alchemystudio/lang/processbuilder/Java9Pipeline.java
@@ -1,7 +1,25 @@
 package io.alchemystudio.lang.processbuilder;
 
-public class Java9Pipeline {
-    public static void main(String[] args) {
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
+public class Java9Pipeline {
+    public static void main(String[] args) throws Exception {
+        // $ ls /tmp | xargs echo
+        List pipe = Arrays.asList(
+                new ProcessBuilder("ls", "/tmp"),
+                new ProcessBuilder("xargs", "echo"));
+
+        List processes = ProcessBuilder.startPipeline(pipe);
+
+        Process last = (Process) processes.get(processes.size() - 1);
+
+        last.waitFor();
+
+        InputStream is = last.getInputStream();
+
+        // 直接输出到`System.out`
+        is.transferTo(System.out);
     }
 }
