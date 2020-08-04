1adfd8ba73b1e90685fbfd471f2a5af0f16ae6aa
阿男 <l.weinan@gmail.com>
2020-08-04 22:03:07 +0800

    jsonb

---
+++ src/main/java/io/alchemystudio/jsonb/Play.java
@@ -0,0 +1,29 @@
+package io.alchemystudio.jsonb;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Play {
+
+    @Getter
+    @Setter
+    public static class Shape {
+        int area;
+        String type;
+    }
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        shape.setArea(12);
+        shape.setType("RECTANGLE");
+        Jsonb jsonb = JsonbBuilder.create();
+// serialize an object to JSON
+        String jsonString = jsonb.toJson(shape); // output : {"area" : 12, "type" : "RECTANGLE"}
+// deserialize a JSON string to an object
+
+        Shape s = jsonb.fromJson("{\"area\" : 12, \"type\": \"TRIANGLE\"}", Shape.class);
+        System.out.println(s);
+    }
+}
