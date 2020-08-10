298b0e45c3a790f230068ac0465328af55fa267f
阿男 <l.weinan@gmail.com>
2020-08-10 18:42:07 +0800

    .

---
+++ src/main/java/io/alchemystudio/lang/TestURI.java
@@ -0,0 +1,18 @@
+package io.alchemystudio.lang;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestURI {
+    public static void main(String[] args) {
+        URI uri = URI.create("https://127.0.0.1:8080/test");
+        System.out.println(uri.getHost());
+        System.out.println(uri.getPath());
+
+        List<String> noProxyHosts = Arrays.asList("localhost|127.*|[::1]".split("\\|"));
+        for (String v : noProxyHosts) {
+            System.out.println(v);
+        }
+    }
+}
