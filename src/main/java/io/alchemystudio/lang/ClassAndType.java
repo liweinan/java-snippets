794907dc748918488211e91be9a1ec01d6bef5b0
阿男 <l.weinan@gmail.com>
2020-12-29 11:20:29 +0800

    LC

---
+++ src/main/java/io/alchemystudio/lang/ClassAndType.java
@@ -0,0 +1,7 @@
+package io.alchemystudio.lang;
+
+public class ClassAndType {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/io/alchemystudio/lc/LC1114.java
@@ -0,0 +1,50 @@
+package io.alchemystudio.lc;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LC1114 {
+    static class Foo {
+        private CountDownLatch m1 = new CountDownLatch(1);
+        private CountDownLatch m2 = new CountDownLatch(1);
+
+        public Foo() {
+
+        }
+
+        public void first(Runnable printFirst) throws InterruptedException {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            m1.countDown();
+        }
+
+        public void second(Runnable printSecond) throws InterruptedException {
+
+            m1.await();
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            m2.countDown();
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+           m2.await();
+
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Foo f = new Foo();
+        f.first(() -> println("A"));
+        f.second(() -> println("B"));
+        f.third(() -> println("C"));
+    }
+
+    private static void println(String a) {
+        System.out.println(a);
+    }
+}
