49b21136ef6d303f415fc998da6565df84a8798a
阿男 <l.weinan@gmail.com>
2021-01-07 22:34:34 +0800

    union find

---
+++ src/main/java/io/weli/lc/UnionFind.java
@@ -0,0 +1,40 @@
+package io.weli.lc;
+
+public class UnionFind {
+    private int count;
+    private int[] parent;
+
+    public UnionFind(int n) {
+        this.count = n;
+        parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int root_p = find(p);
+        int root_q = find(q);
+
+        if (root_p == root_q)
+            return;
+
+        parent[root_p] = root_q;
+
+        count--;
+    }
+
+    private int find(int x) {
+        while (parent[x] != x) {
+            x = parent[x];
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(5);
+        uf.union(1, 2);
+
+    }
+}
