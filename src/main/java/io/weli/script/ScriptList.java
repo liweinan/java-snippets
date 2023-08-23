790b70ae25f1cc5d4082dc41c14ee541c54a0e7b
阿男 <l.weinan@gmail.com>
2023-08-23 21:36:09 +0800

    register python script

---
+++ src/main/java/io/weli/script/ScriptList.java
@@ -0,0 +1,25 @@
+package io.weli.script;
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import java.util.List;
+
+public class ScriptList {
+    public static void main(String[] args) {
+        ScriptEngineManager manager = new ScriptEngineManager();
+
+        // Get the list of all available engines
+        List<ScriptEngineFactory> list = manager.getEngineFactories();
+
+        // Print the details of each engine
+        for (ScriptEngineFactory f : list) {
+            System.out.println("Engine Name:" + f.getEngineName());
+            System.out.println("Engine Version:" + f.getEngineVersion());
+            System.out.println("Language Name:" + f.getLanguageName());
+            System.out.println("Language Version:" + f.getLanguageVersion());
+            System.out.println("Engine Short Names:" + f.getNames());
+            System.out.println("Mime Types:" + f.getMimeTypes());
+            System.out.println("===");
+        }
+    }
+}
