ed6b380bd78dc8cf13912dee839afed712af301b
阿男 <l.weinan@gmail.com>
2023-10-28 01:50:05 +0800

    .

--- src/main/java/io/weli/commons/ProcessProps.java
+++ src/main/java/io/weli/commons/ProcessProps.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class ProcessProps {
 
-    static final String PATTERN = "\\$\\{([0-9a-zA-Z_\\-]+):([0-9a-zA-Z_\\-]+)\\}";
+    static final String PATTERN = "\\$\\{([0-9a-zA-Z_\\-]+)(:([0-9a-zA-Z_\\-]*))?\\}";
     public static void main(String[] args) {
 
         var prop_val1 = "${x:y}";
@@ -13,12 +13,16 @@ public class ProcessProps {
         var prop_val4 = "${{x:y}";
         var prop_val5 = "${x:y}";
         var prop_val6 = "${DB_USER:admin}";
+        var prop_val7 = "${DB_PASS:}"; // no default value
+        var prop_val8 = "${FOO:}"; // no default value
         verifyStr(prop_val1);
         verifyStr(prop_val2);
         verifyStr(prop_val3);
         verifyStr(prop_val4);
         verifyStr(prop_val5);
         verifyStr(prop_val6);
+        verifyStr(prop_val7);
+        verifyStr(prop_val8);
     }
 
     private static void verifyStr(String propVal) {
@@ -29,9 +33,10 @@ public class ProcessProps {
             System.out.println("group(0): " + matcher.group(0) + " pos: " + matcher.start());
             System.out.println("group(1): " + matcher.group(1) + " pos: " + matcher.start());
             System.out.println("group(2): " + matcher.group(2) + " pos: " + matcher.start());
+            System.out.println("group(3): " + matcher.group(3) + " pos: " + matcher.start());
 
             System.out.println("k -> " + matcher.group(1));
-            System.out.println("v -> " + matcher.group(2));
+            System.out.println("v -> " + matcher.group(3));
         }
     }
 
