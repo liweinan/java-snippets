7214c3f1d40ae7db502652c8dd86dddd9d633191
阿男 <l.weinan@gmail.com>
2025-03-14 21:26:24 +0800

    save progress

---
+++ src/main/java/io/weli/math/Mod.java
@@ -0,0 +1,15 @@
+package io.weli.math;
+
+import java.util.List;
+
+public class Mod {
+    public static void main(String[] args) {
+        System.out.println(1 % 2);
+        System.out.println(0 % 2);
+
+        List<Integer>[] arr = new List[10];
+
+        System.out.println(String.valueOf("abcdefg"));
+
+    }
+}
