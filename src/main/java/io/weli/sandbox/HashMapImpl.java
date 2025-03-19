cedb66558bf0a66d0ca60cfb71279876f97e332f
阿男 <l.weinan@gmail.com>
2025-03-19 18:05:28 +0800

    save progress

---
+++ src/main/java/io/weli/sandbox/HashMapImpl.java
@@ -0,0 +1,9 @@
+package io.weli.sandbox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapImpl {
+    public static void main(String[] args) {}
+    Map m = new HashMap<>();
+}
