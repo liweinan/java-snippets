65b875f2e7037a1f033f15928fcf234d2b277510
阿男 <l.weinan@gmail.com>
2025-03-19 18:36:22 +0800

    save progress

---
+++ src/main/java/io/weli/sandbox/JavaStringTokens.java
@@ -0,0 +1,19 @@
+package io.weli.sandbox;
+
+import java.util.Arrays;
+
+// https://www.hackerrank.com/challenges/java-string-tokens/problem
+public class JavaStringTokens {
+    public static void main(String[] args) {
+//        String in = "He is a very very good boy, isn't he?";
+//        String in = "           YES      leading spaces        are valid,    problemsetters are         evillllll";
+        String in = "                        ";
+        if (in.trim().equals("")) {
+            System.out.println("0");
+        } else {
+            String[] out = in.trim().split("[ !,?._'@]+");
+            System.out.println(out.length);
+            Arrays.stream(out).forEach(System.out::println);
+        }
+    }
+}
