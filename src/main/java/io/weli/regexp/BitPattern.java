f1ec748bdfddf7b221f250177ba9255fa80596f3
阿男 <l.weinan@gmail.com>
2025-03-23 19:19:21 +0800

    save progress

---
+++ src/main/java/io/weli/regexp/BitPattern.java
@@ -0,0 +1,8 @@
+package io.weli.regexp;
+
+public class BitPattern {
+    public static void main(String[] args) {
+        System.out.println(Integer.toHexString(1000));
+        System.out.println(Integer.toBinaryString(1000));
+    }
+}
