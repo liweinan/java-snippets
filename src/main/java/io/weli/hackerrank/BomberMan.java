9c8a3943f0806441ee246bec454c1a12d3a9ccc3
阿男 <l.weinan@gmail.com>
2025-03-25 00:38:48 +0800

    save progress

---
+++ src/main/java/io/weli/hackerrank/BomberMan.java
@@ -0,0 +1,30 @@
+package io.weli.hackerrank;
+
+import java.util.List;
+
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-bomber-man/problem
+// https://github.com/RyanFehr/HackerRank/blob/master/Algorithms/Implementation/The%20Bomberman%20Game/Solution.java
+public class BomberMan {
+
+    /*
+     * Complete the 'bomberMan' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. STRING_ARRAY grid
+     */
+
+    // cycle 3 and cycle 5 are the same
+    // i % 2 == 0
+    // full map are bombs
+    public static List<String> bomberMan(int n, List<String> grid) {
+        // Write your code here
+        // n % 2
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
