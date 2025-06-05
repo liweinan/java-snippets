846728c3cded6d26b70a0b8100f5eda3abda79dc
阿男 <l.weinan@gmail.com>
2025-06-05 22:37:55 +0800

    BalancedParentheses

---
+++ src/main/java/io/weli/testdome/BalancedParentheses.java
@@ -0,0 +1,69 @@
+package io.weli.testdome;
+
+import java.util.*;
+
+// https://www.testdome.com/library?page=1&skillArea=30&questionId=135032
+public class BalancedParentheses {
+
+    public static boolean isBalanced(String parentheses) {
+        Stack<Character> stack = new Stack<>();
+        Set<Character> opening = new HashSet<>(Arrays.asList('(', '[', '{'));
+        Set<Character> closing = new HashSet<>(Arrays.asList(')', ']', '}'));
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+
+        for (char c : parentheses.toCharArray()) {
+            System.out.println("current c: " + c);
+            if (opening.contains(c)) {
+                stack.push(c);
+            } else if (closing.contains(c)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isBalanced2(String parentheses) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+
+        for (char c : parentheses.toCharArray()) {
+            System.out.println("current c: " + c);
+            if (pairs.containsValue(c)) {
+                stack.push(c);
+            } else if (pairs.containsKey(c)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String[] expressions = {
+            "(3+5)*(2-4)",
+            "{[()]}",
+            "[3+5x(4-1]-39]"
+        };
+
+        for (String expr : expressions) {
+            System.out.printf("Expression: %s\tBalanced: %b\n", expr, isBalanced(expr));
+        }
+
+        System.out.println("\n----------------------------------------\n");
+
+        for (String expr : expressions) {
+            System.out.printf("Expression: %s\tBalanced2: %b\n", expr, isBalanced2(expr));
+        }
+    }
+}
